a0f59b243290e3498d580e19bb16dde828ca0139
Miguel Angel Luna <miluna1994@gmail.com>
2019-01-31 20:06:14 +0100

    finished hello world

--- react-helloworld/src/App.js
+++ react-helloworld/src/App.js
@@ -1,43 +1,53 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import HelloWorld from "./components/HelloWorld";
 
 class App extends Component {
-  state = {
-      identificador: "5",
-      texto: ""
-  };
+    constructor(props) {
+        super(props);
+        this.state = {
+            identificador: "5",
+            texto: ""
+        };
+        this.getInput = this.getInput.bind(this);
+    }
 
+    getInput(e) {
+        const textoIntroducido = e.target.value;
+        this.setState({texto: textoIntroducido});
+    }
 
-  componentDidMount() {
-    this.setState({texto: "Hello World"});
-  }
+    componentDidMount() {
+        this.setState({texto: "Hello World"});
+    }
 
     render() {
-    const {texto, identificador} = this.state;
+        const {texto, identificador} = this.state;
 
-    const estiloDiv = {
-        position: 'absolute',
-        top: '5rem',
-        left:'0',
-        right: '0',
-        margin: 'auto',
-        width: '20rem',
-        boxShadow: '0.5rem 0.4rem 2rem #888888',
-        padding: '3rem'
-    };
+        const estiloDiv = {
+            position: 'absolute',
+            top: '5rem',
+            left: '0',
+            right: '0',
+            margin: 'auto',
+            width: '20rem',
+            boxShadow: '0.5rem 0.4rem 2rem #888888',
+            borderRadius: '1rem',
+            padding: '3rem'
+        };
 
-    return (
-        <div>
-            <div style={estiloDiv}>
-                <label>Introduce lo que quieres pasar al componente</label>
-                <br/>
-                <input type="text" />
-                <h2>Texto introducido:</h2>
-                <HelloWorld id={identificador} textoProp={texto} />
+        return (
+            <div>
+                <div style={estiloDiv}>
+                    <h1>Hello World App</h1>
+                    <label>Introduce lo que quieres pasar al componente</label>
+                    <br/>
+                    <input onChange={this.getInput} type="text" placeholder="Introduce texto aqui"/>
+                    <h2>Texto introducido:</h2>
+                    <HelloWorld id={identificador} textoProp={texto}/>
+                </div>
             </div>
-        </div>
-    );
-  }
+        );
+    }
 }
 
 export default App;
--- react-helloworld/src/components/HelloWorld.js
+++ react-helloworld/src/components/HelloWorld.js
@@ -1,10 +1,11 @@
 import React from 'react';
+import './HelloWorld.css';
 
 const HelloWorld = (props) => {
     const {id, textoProp } = props;
 
     return (
-      <div id={id}>
+      <div className="hello-world" id={id}>
           <p>{textoProp}</p>
       </div>
     );
